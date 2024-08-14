28fbddd0c11014132ff02caaf6072de4f2386516
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-14 07:43:50 +0100

    Create HelloController and update App class with CommandLineRunner bean, add test using MockMvc & TestTemplate

--- src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
+++ src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
@@ -1,7 +1,10 @@
 package com.example.spring_boot_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
@@ -10,4 +13,15 @@ public class SpringBootAppApplication {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String[] beans = ctx.getBeanDefinitionNames();
+
+			for (String bean : beans) {
+				System.out.println(bean);
+			}
+		};
+	}
 }
---
+++ src/test/java/com/example/spring_boot_app/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.spring_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void GetHelloMockMvc() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+
+    @Test
+    public void GetHelloRestTemplate() throws Exception {
+        String response = restTemplate.getForObject("/", String.class);
+        assert response.equals("Hello World!");
+    }
+}
